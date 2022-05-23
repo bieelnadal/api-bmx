0ec81e3b6343eb72a67351177df94d6c038732aa
bieelnadal <bielito1@gmail.com>
2022-05-23 22:19:04 +0200

    a

--- direcciones/establecerPredeterminado.php
+++ direcciones/establecerPredeterminado.php
@@ -1,7 +1,7 @@
 <?php
 // headers
 header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Methods: *");
+header("Access-Control-Allow-Methods:PU*");
 header("Access-Control-Allow-Headers: Authorization, Origin, X-Requested-With, Content-Type, Accept");
 
 // includes
